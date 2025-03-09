a57314636650a99537d9b0fc8025995e6001f462
Shoaib Ahmed Bappy <shoaibahmedbappy@gmail.com>
2025-03-09 12:57:55 +0600

    Add files via upload

---
+++ wp-admin/admin-ajax.php
@@ -0,0 +1,16 @@
+			<p>Cart is empty.            </p>
+			
+					<li class="d-flex justify-content-between align-items-center cart-item">
+				<h3 class="fs-15 mb-0">Total</h3>
+				<h3 class="fs-15 mb-0"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#036;</span>0</span></h3>
+			</li>
+		
+		<li class="d-flex justify-content-between align-items-center cart-item">
+			
+							<a href="https://themes.envytheme.com/liva/cart/" class="btn btn-primary fs-14 text-white">
+					View Cart				</a>
+			
+							<a href="https://themes.envytheme.com/liva/checkout/" class="btn btn-warning fs-14 text-white">
+					Checkout				</a>
+					</li>
+		
